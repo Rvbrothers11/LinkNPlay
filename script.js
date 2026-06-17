404b95d4c19b1c39103fac3b94eb259cdc3419a0
Vidit <viditbhati@gmail.com>
2026-06-17 22:07:01 +0530

    edited the script for new variables, and winning combinations for tic tac toe.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const socket = io();
 
+let currentRoom = "";
+let mySymbol = "";
+let isMyTurn = false;
+let boardState = ["", "", "", "", "", "", "", "", ""];
+
+const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]
+];
+
 document.addEventListener("DOMContentLoaded", () => {
     let username = localStorage.getItem("linkNPlayUser");
 
