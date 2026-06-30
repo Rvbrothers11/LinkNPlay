6b49dbbb1098e2eb15498cb88453f373a601650f
Vidit <viditbhati@gmail.com>
2026-06-30 10:03:06 +0530

    finally got the server to work.

--- server.js
+++ server.js
@@ -115,4 +115,4 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
-}); 
+});
